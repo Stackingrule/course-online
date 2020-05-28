6a38c341fad6e06ccaf7ed8bffaf85fa390051ff
Stackingrule <messageswitching@gmail.com>
2020-05-28 20:39:54 +0800

    1.简化登录页面代码 2.增加eslint.js，去掉部分检查项 解决修改代码自动编译报错: $ is not defined

--- course/admin/.eslintrc.js
+++ course/admin/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-undef': 'off',
+    'vue/no-unused-vars': 'off',
+    'vue/require-v-for-key': 'off',
   }
 }
