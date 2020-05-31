1557594530d928d76c294cef77844d14d534ed3f
Stackingrule <messageswitching@gmail.com>
2020-05-31 21:18:34 +0800

    1.增加新增大章功能，后端代码开发

--- course/business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ course/business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,5 +1,6 @@
 package com.course.business.controller.admin;
 
+import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
 import org.slf4j.Logger;
@@ -23,5 +24,11 @@ public class ChapterController {
     public PageDto list(@RequestBody PageDto pageDto) {
         chapterService.list(pageDto);
         return pageDto;
+    }
+
+    public ChapterDto save(@RequestBody ChapterDto chapterDto) {
+        LOG.info("chapterDto:{}", chapterDto);
+        chapterService.save(chapterDto);
+        return chapterDto;
     }
 }
