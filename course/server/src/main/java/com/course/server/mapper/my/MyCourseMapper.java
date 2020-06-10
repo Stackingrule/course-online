f2f4c7bc69693f287b12bf23fd83d17a0c88a3f5
Stackingrule <messageswitching@gmail.com>
2020-06-10 21:35:30 +0800

    1.保存时长，更新课程总时长 2.增加自定义mapper

---
+++ course/server/src/main/java/com/course/server/mapper/my/MyCourseMapper.java
@@ -0,0 +1,20 @@
+package com.course.server.mapper.my;
+
+import com.course.server.dto.CourseDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface MyCourseMapper {
+
+//    List<CourseDto> list(@Param("pageDto") CoursePageDto pageDto);
+
+    int updateTime(@Param("courseId") String courseId);
+//
+//    int updateSort(SortDto sortDto);
+//
+//    int moveSortsBackward(SortDto sortDto);
+//
+//    int moveSortsForward(SortDto sortDto);
+
+}
